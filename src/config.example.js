109212f659614aa0e4dc923f79f9e01e62d414a7
Stijn Elskens <stijn.elskens@gmail.com>
2020-09-18 08:51:28 +0200

    Update config example ✨

--- src/config.example.js
+++ src/config.example.js
@@ -1,8 +1,8 @@
 const config = {
-  bridgeId: 'bMoUF4kyaFhjfOi90mUzNlXrDJ4dw8c40SyFkwmJ',
-  lightId: '19',
-  groupId: '9',
-  channelName: 'laks_1',
+  bridgeId: '',
+  lightId: '',
+  groupId: '',
+  channelName: 'channel-name',
 };
 
 module.exports.config = config;
